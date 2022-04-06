b0648909b68c354aa88f0861fc87b1d1177551a1
EDUARDO CHAVEZ <41527710+RainbowChvz@users.noreply.github.com>
2022-04-05 17:39:55 -0700

    Repurpose status property to display a loading or error image when necessary

--- app/src/main/java/com/example/android/marsphotos/BindingAdapters.kt
+++ app/src/main/java/com/example/android/marsphotos/BindingAdapters.kt
@@ -1,11 +1,13 @@
 package com.example.android.marsphotos
 
+import android.view.View
 import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import com.example.android.marsphotos.network.MarsPhoto
+import com.example.android.marsphotos.overview.MarsApiStatus
 import com.example.android.marsphotos.overview.PhotoGridAdapter
 
 @BindingAdapter("imageUrl")
@@ -23,8 +25,24 @@ fun bindImage(imgView: ImageView, imgUrl: String?) {
         }
     }
 }
+
 @BindingAdapter("listData")
 fun bindRecyclerView(recView: RecyclerView, photosList: List<MarsPhoto>?) {
     val adapter = recView.adapter as PhotoGridAdapter
     adapter.submitList(photosList)
+}
+
+@BindingAdapter("marsApiStatus")
+fun bindStatus(imgView: ImageView, status: MarsApiStatus?) {
+    when (status) {
+        MarsApiStatus.LOADING -> {
+            imgView.visibility = View.VISIBLE
+            imgView.setImageResource(R.drawable.loading_animation)
+        }
+        MarsApiStatus.ERROR -> {
+            imgView.visibility = View.VISIBLE
+            imgView.setImageResource(R.drawable.ic_connection_error)
+        }
+        else -> imgView.visibility = View.GONE
+    }
 }
